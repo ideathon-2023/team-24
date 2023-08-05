1126a9dd6ed3d433eb06fcfddc68bd6bc72408bc
himanshusab12 <119474384+himanshusab12@users.noreply.github.com>
2023-08-05 14:15:49 +0530

    Add files via upload

---
+++ frontend/student_end/campusPlates.js
@@ -0,0 +1,47 @@
+let ele = document.getElementsByClassName("signup")[0];
+let ele2 = document.getElementsByClassName("signup")[1];
+ele.addEventListener("click",function(){
+    window.location.href = "campusplatesLogin.html";
+})
+ele2.addEventListener("click",function(){
+    window.location.href = "campusplatesLogin.html";
+})
+function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+      testAPI();  
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+    }
+  }
+
+
+  function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function(response) {   // See the onlogin handler
+      statusChangeCallback(response);
+    });
+  }
+
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '833392331343993',
+      cookie     : true,                     // Enable cookies to allow the server to access the session.
+      xfbml      : true,                     // Parse social plugins on this webpage.
+      version    : 'v3.2'           // Use this Graph API version for this call.
+    });
+
+
+    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
+      statusChangeCallback(response);        // Returns the login status.
+    });
+  };
+ 
+  function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
