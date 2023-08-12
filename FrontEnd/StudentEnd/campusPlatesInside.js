44e0e07a578c99ea3439073bbfb284a982bba97a
Himanshu Sabharwal <119474384+himanshusab12@users.noreply.github.com>
2023-08-12 12:12:15 +0530

    Add files via upload

--- FrontEnd/StudentEnd/campusPlatesInside.js
+++ FrontEnd/StudentEnd/campusPlatesInside.js
@@ -1,10 +1,14 @@
 let ele = document.getElementsByClassName("colBlock");
 let rem = document.getElementsByClassName("colBlock");
-for (let element of ele) {
-    element.addEventListener("click", function () {
-        element.classList.toggle("active");
+let dbox1 = document.getElementsByClassName("circl1")[0];
+let dbox2 = document.getElementsByClassName("circl2")[0];
+let dbox3 = document.getElementsByClassName("circl3")[0];
+let dbox4 = document.getElementsByClassName("circl4")[0];
+for (let bfastnt of ele) {
+    bfastnt.addEventListener("click", function () {
+        bfastnt.classList.toggle("active");
         for (let remov of rem) {
-            if (element != remov)
+            if (bfastnt != remov)
                 remov.classList.remove("active");
         }
     })
@@ -25,34 +29,56 @@ let home = document.getElementsByClassName("home")[0];
 home.addEventListener("click", function () {
     window.location.href = "/HomePage/index.html";
 })
-ele[2].addEventListener("click",function(){
+ele[2].addEventListener("click", function () {
     let counts = setInterval(updated);
-            let upto1 = 1;
-            let upto2 = 1;
-            let upto3 = 1;
-            let upto4 = 1;
-            function updated() {
-                let count1 = document.getElementsByClassName("cntr")[0];
-                count1.innerHTML = ++upto1;
-                if (upto1 === 1000) {
-                    clearInterval(counts);
-                }
-                let count2 = document.getElementsByClassName("cntr")[1];
-                count2.innerHTML = ++upto2;
-                if (upto2 === 1000) {
-                    clearInterval(counts);
-                }
-                let count3 = document.getElementsByClassName("cntr")[2];
-                count3.innerHTML = ++upto3;
-                if (upto3 === 1000) {
-                    clearInterval(counts);
-                }
-                let count4 = document.getElementsByClassName("cntr")[3];
-                count4.innerHTML = ++upto4;
-                if (upto4 === 1000) {
-                    clearInterval(counts);
-                }
-            }
+    let upto1 = 1;
+    function updated() {
+        let count1 = document.getElementsByClassName("cntr")[0];
+        count1.innerHTML = ++upto1;
+        if (upto1 === 1000) {
+            clearInterval(counts);
+        }
+    }
+}, 2000)
+let bfast = document.getElementsByClassName("cntr1")[0];
+dbox1.addEventListener("click", function () {
+    if (bfast.innerHTML === "Breakfast Fees") {
+        bfast.innerHTML = "1000";
+    }
+    else {
+        bfast.innerHTML = "Breakfast Fees";
+
+    }
+})
+let lunch = document.getElementsByClassName("cntr2")[0];
+dbox2.addEventListener("click", function () {
+    if (lunch.innerHTML === "Lunch Fees") {
+        lunch.innerHTML = "1000";
+    }
+    else {
+        lunch.innerHTML = "Lunch Fees";
+
+    }
+})
+let esnacks = document.getElementsByClassName("cntr3")[0];
+dbox3.addEventListener("click", function () {
+    if (esnacks.innerHTML === "Snacks Fees") {
+        esnacks.innerHTML = "1000";
+    }
+    else {
+        esnacks.innerHTML = "Snacks Fees";
+
+    }
+})
+let dinner = document.getElementsByClassName("cntr4")[0];
+dbox4.addEventListener("click", function () {
+    if (dinner.innerHTML === "Dinner Fees") {
+        dinner.innerHTML = "1000";
+    }
+    else {
+        dinner.innerHTML = "Dinner Fees";
+
+    }
 })
 document.addEventListener('DOMContentLoaded', function () {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
