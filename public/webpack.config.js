1abaecc186fdd423d5f7ddd57d2607d496617ee2
Aaditya <aadityavardhanvij.bt22cse@pec.edu.in>
2023-08-16 16:37:33 +0530

    hosting

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -26,16 +26,30 @@ module.exports = {
     },
     watch: true
 }
-// module.exports = {
-//     mode: 'development',
-//     entry: {
-//         // bundle1: './BackEnd/index.js',
-//         bundle3: './BackEnd/index3.js',
-//       },
-//     output: {
-//         path: path.resolve(__dirname, 'FrontEnd'),
-//         // filename: 'bundle.js',
-//         filename: 'bundle3.js'
-//     },
-//     watch: true
-// }
+module.exports = {
+    mode: 'development',
+    entry: {
+        // bundle1: './BackEnd/index.js',
+        bundle3: './BackEnd/index3.js',
+      },
+    output: {
+        path: path.resolve(__dirname, 'FrontEnd'),
+        // filename: 'bundle.js',
+        filename: 'bundle3.js'
+    },
+    watch: true
+}
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        // bundle1: './BackEnd/index.js',
+        bundle4: './BackEnd/index4.js',
+      },
+    output: {
+        path: path.resolve(__dirname, 'FrontEnd'),
+        // filename: 'bundle.js',
+        filename: 'bundle4.js'
+    },
+    watch: true
+}
